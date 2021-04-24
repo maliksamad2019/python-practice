b66cd8db1f0dae83cbda74b766cf907e88c36117
MalikSamad <maliksamad2019@gmail.com>
2021-04-24 19:47:32 +0500

    madlib done

---
+++ 1-madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = "Malik@youtub"
+# print("subscribe to "+ youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("adjective: ")
+verb1 = input("verb-1: ")
+verb2 = input("verb-2: ")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
